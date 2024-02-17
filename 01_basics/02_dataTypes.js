aed9a676d874d10d75390c0899523e5ef4ae050c
Amoghavarsha <118446510+Amoghavarshab@users.noreply.github.com>
2024-02-17 02:18:13 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all the JS code as newer version
+
+//alert(3+3) //we are using nodejs , not browser
+
+console.log(3+3) //code readability should be high
+console.log("amogh")
+
+let name="amogh"
+let age=20
+let isloggedIn=false
+let state;
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>  value not assigned
+//symbol => unique
+
+
+//object
+
+console.log(typeof null); // null is object
+console.log(typeof undefined); //undefined
+console.log(typeof "amogh");
+console.log(typeof age);
